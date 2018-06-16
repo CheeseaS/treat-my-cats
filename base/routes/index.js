63bbb6f57dd58105d9161dfec0df987f9638a7b2
Chelsea Simpson <cheesesicklesalad@gmail.com>
2018-06-15 19:34:22 -0700

    I hope I didn't break it

--- base/routes/index.js
+++ base/routes/index.js
@@ -1,10 +1,42 @@
 var express = require('express');
 var router = express.Router();
+var rootPath = '/home/pi/treat-my-cats/base/public/';
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
     res.status(200);
-    res.sendFile('/html/index.html', {'root': '/home/pi/treat-my-cats/base/public'});
+    res.sendFile('/html/index.html', {'root': rootPath});
 });
 
+router.post('/', function(req, res){
+    var stripe = require("stripe")("sk_test_H3QtUiTsqzlce4TVCkNpMmpk");
+    var PythonShell = require('python-shell');
+
+    const token = req.body.stripeToken;
+    const charge = stripe.charges.create({
+	amount: 50,
+	currency: 'usd',
+	description: 'gave cats treats',
+	source: token,
+    });
+
+    if(typeof charge != "undefined") {
+	var PythonShell = require('python-shell');
+
+	PythonShell.run('treatCat.py', function (err) {
+	    if (err) throw err;
+	    console.log('finished');
+	});
+	console.log("gave treat");
+    }
+    res.sendFile('/html/index.html', {'root': rootPath});
+});
+
+router.get('/get-page*', function(req, res, next) {
+    var id=req.url.split('=')[1];
+console.log(id);
+    res.status(200);
+    res.type('html');
+    res.sendFile('/html/' + id + '.html', {'root': rootPath})
+});
 module.exports = router;
