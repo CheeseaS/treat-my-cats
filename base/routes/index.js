9de7a32fe4fc231ffdc02c4732896ac4fa1d6dbe
Chelsea Simpson <cheesesicklesalad@gmail.com>
2019-06-10 20:21:31 -0700

    treat my cats site, less cert

--- base/routes/index.js
+++ base/routes/index.js
@@ -8,30 +8,18 @@ router.get('/', function(req, res, next) {
     res.sendFile('/html/index.html', {'root': rootPath});
 });
 
-router.post('/', function(req, res){
-    var stripe = require("stripe")("sk_test_H3QtUiTsqzlce4TVCkNpMmpk");
+/* POST feed treats */
+router.post('/dispense', function(req, res){
     var PythonShell = require('python-shell');
-
-    const token = req.body.stripeToken;
-    const charge = stripe.charges.create({
-	amount: 50,
-	currency: 'usd',
-	description: 'gave cats treats',
-	source: token,
-    });
-
-    if(typeof charge != "undefined") {
-	var PythonShell = require('python-shell');
-
+  /* the python script that activates the servo */  
 	PythonShell.run('treatCat.py', function (err) {
 	    if (err) throw err;
 	    console.log('finished');
 	});
 	console.log("gave treat");
-    }
-    res.sendFile('/html/index.html', {'root': rootPath});
 });
 
+/* GET page content */
 router.get('/get-page*', function(req, res, next) {
     var id=req.url.split('=')[1];
 console.log(id);
